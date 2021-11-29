b79ae041a1787ab68ee3819ac539591e3b58c981
Kinh Nguyen <4459378+kklot@users.noreply.github.com>
2021-11-29 16:30:39 +0100

    merge with fuchs

--- sim.sh
+++ sim.sh
@@ -1,23 +1,28 @@
 #!/bin/bash
+#SBATCH --job-name=dbmt
+#SBATCH --partition=fuchs
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=40
+#SBATCH --mem-per-cpu=3000
+#SBATCH --mail-type=ALL
+#SBATCH --time=32:00:00
 
-Rscript -e 'rmarkdown::render("sim_fit.Rmd","html_document","o21000nonenone",params=list(nsv =2,sample_size=1000,bias="none",trend="none"))'
-Rscript -e 'rmarkdown::render("sim_fit.Rmd","html_document","o21000noneincrease",params=list(nsv =2,sample_size=1000,bias="none",trend="increase"))'
-Rscript -e 'rmarkdown::render("sim_fit.Rmd","html_document","o21000nonedecrease",params=list(nsv =2,sample_size=1000,bias="none",trend="decrease"))'
+NSV=(2 3 4)
+SAMPLE=(1000) 
+BIAS=(men women none)
+TREND=(none increase decrease)
 
-Rscript -e 'rmarkdown::render("sim_fit.Rmd","html_document","o31000nonenone",params=list(nsv =3,sample_size=1000,bias="none",trend="none"))'
-Rscript -e 'rmarkdown::render("sim_fit.Rmd","html_document","o31000noneincrease",params=list(nsv =3,sample_size=1000,bias="none",trend="increase"))'
-Rscript -e 'rmarkdown::render("sim_fit.Rmd","html_document","o31000nonedecrease",params=list(nsv =3,sample_size=1000,bias="none",trend="decrease"))'
-
-Rscript -e 'rmarkdown::render("sim_fit.Rmd","html_document","o21000logisnone",params=list(nsv =2,sample_size=1000,bias="logis",trend="none"))'
-Rscript -e 'rmarkdown::render("sim_fit.Rmd","html_document","o21000logisincrease",params=list(nsv =2,sample_size=1000,bias="logis",trend="increase"))'
-Rscript -e 'rmarkdown::render("sim_fit.Rmd","html_document","o21000logisdecrease",params=list(nsv =2,sample_size=1000,bias="logis",trend="decrease"))'
-
-Rscript -e 'rmarkdown::render("sim_fit.Rmd","html_document","o31000logisnone",params=list(nsv =3,sample_size=1000,bias="logis",trend="none"))'
-Rscript -e 'rmarkdown::render("sim_fit.Rmd","html_document","o31000logisincrease",params=list(nsv =3,sample_size=1000,bias="logis",trend="increase"))'
-Rscript -e 'rmarkdown::render("sim_fit.Rmd","html_document","o31000logisdecrease",params=list(nsv =3,sample_size=1000,bias="logis",trend="decrease"))'
-
-# 4 surveys
-Rscript -e 'rmarkdown::render("sim_fit.Rmd","html_document","o41000logisnone",params=list(nsv =4,sample_size=1000,bias="logis",trend="none"))'
-Rscript -e 'rmarkdown::render("sim_fit.Rmd","html_document","o41000logisincrease",params=list(nsv =4,sample_size=1000,bias="logis",trend="increase"))'
-Rscript -e 'rmarkdown::render("sim_fit.Rmd","html_document","o41000logisdecrease",params=list(nsv =4,sample_size=1000,bias="logis",trend="decrease"))'
-
+for sps in ${SAMPLE[@]} 
+do
+	for td in ${TREND[@]}
+	do
+		for nsv in ${NSV[@]} 
+		do
+			for ba in ${BIAS[@]} 
+			do
+				srun Rscript -e "rmarkdown::render(\"sim_fit.Rmd\",\"html_document\", \"o${nsv}${sps}${ba}${td}\", \"html\", params=list(nsv=${nsv},sample_size=${sps},bias=\"${ba}\",trend=\"${td}\"))"
+			done
+		done
+	done
+done
+exit 0
